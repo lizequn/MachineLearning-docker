293009d9c1893671085805c41d8680bd12f3d80d
zequnli <lzq910123@gmail.com>
2017-12-17 10:53:39 +0800

    docker env

---
+++ docker_install.sh
@@ -0,0 +1,19 @@
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+
+apt-get install docker-ce
+
+apt-get install nvidia-docker2
+pkill -SIGHUP dockerd
