c773a9a5e63e0d85d1aeef039c64842ea8ac68ca
zequnli <lzq910123@gmail.com>
2017-12-17 11:02:17 +0800

    nvidia docker

--- docker_install.sh
+++ docker_install.sh
@@ -1,11 +1,11 @@
 apt-get update
-apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -13,7 +13,11 @@ add-apt-repository \
    stable"
 apt-get update
 
-apt-get install docker-ce
+apt-get install -y docker-ce
 
-apt-get install nvidia-docker2
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+apt-get update
+
+apt-get install -y nvidia-docker2
 pkill -SIGHUP dockerd
